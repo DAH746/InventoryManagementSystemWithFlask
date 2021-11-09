e0a2dd18d51e8b79598f65d77eeae9f4316acfd1
gcaddick <gcaddick@softwareinstitute.com>
2021-11-09 06:04:24 -0500

    Added s3 bucket upload to main application

--- WebAppForUsers.py
+++ WebAppForUsers.py
@@ -3,6 +3,8 @@ import sqlite3 as sql
 import random
 import string
 from datetime import datetime
+import boto3
+from werkzeug.utils import secure_filename
 
 """
 Importing serveral packages
@@ -25,6 +27,11 @@ Allows user to list all users for debugging purposes
 """
 
 app = Flask(__name__, template_folder='templates')
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+
+s3 = boto3.client('s3')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+BUCKET_NAME = 'source-image-bucket-5623'
 
 def IsUserNew(email):
     con = sql.connect("UserDatabase.db")
@@ -115,6 +122,31 @@ def check_user_login():
         finally:
             return render_template("result.html",msg = msg)
             con.close()
+
+@app.route('/s3UploadTest')
+def s3_upload():
+    return render_template('s3UploadTest.html')
+
+@app.route('/upload',methods=['post'])
+def upload():
+    if request.method == 'POST':
+        img = request.files['file']
+        if img:
+            filename = secure_filename(img.filename)
+            fileNameSplit = filename.split(".")
+            fileExtention = fileNameSplit[1]
+            if fileExtention in ALLOWED_EXTENSIONS:
+                img.save(filename)
+                s3.upload_file(
+                    Bucket = BUCKET_NAME,
+                    Filename=filename,
+                    Key = filename
+                )
+                msg = "Upload Done ! "
+            else:
+                msg = "Not a png/jpg/jpeg."
+
+    return render_template("result.html",msg =msg)
 
 
 if __name__ == '__main__':
