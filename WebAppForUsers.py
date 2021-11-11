f30de90a58dfa26e9b5be7ffb9ea8af709eacf9a
DAH746 <ahdanyal@googlemail.com>
2021-11-11 10:16:21 +0000

    George's code because he loves git so, so, so very much

--- WebAppForUsers.py
+++ WebAppForUsers.py
@@ -26,43 +26,48 @@ user_id, enail, first and last name, password and address details
 Checks to see if the email already exists in the database.
 Allows user to list all users for debugging purposes
 Includes login function where the the user and upload picture to an S3 bucket to be resized
-New product database. Allows user to add new products (checks to see if the product has already been added)
+New product database. Allows user to add new products (checks to see if the  has already been added)
 and then saves to database. User can also list all products in the database.
 """
 
-app = Flask(__name__, template_folder='templates') #Sets the templates folder for the website
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 # Sets maximum filesize to be uploaded at 1MB
+app = Flask(__name__, template_folder='templates')  # Sets the templates folder for the website
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024  # Sets maximum filesize to be uploaded at 1MB
 
-
-s3 = boto3.client('s3') # Sets up S3 client
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'} # Only allowed extensions allowed for file upload
+s3 = boto3.client('s3')  # Sets up S3 client
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}  # Only allowed extensions allowed for file upload
 BUCKET_NAME = 'source-image-bucket-5623'
+
 # global LOGIN # Global login variable
 
 # START - Instantiate objects
 hasAllBucketNamesObject = bucket_names_object.bucket_names_object()
-containsAllUrlsForEachS3Bucket = contains_all_urls_for_s3_buckets.contains_all_urls_for_s3_buckets(hasAllBucketNamesObject)
+containsAllUrlsForEachS3Bucket = contains_all_urls_for_s3_buckets.contains_all_urls_for_s3_buckets(
+    hasAllBucketNamesObject)
+
+
 # END - Instantiate objects
 
 # Function checks if the email inputted from the user is currently in the database.
 # Queries the user database by selecting emails where the saved email is the same as the user
 # input email. Returns True is user is new or False if already registered user.
 def IsUserNew(email):
-    con = sql.connect("UserDatabase.db") # The database to connect to
+    con = sql.connect("UserDatabase.db")  # The database to connect to
     cur = con.cursor()
-    cur.execute("Select email from Users WHERE email=?",(email, )) # Queries database
-    all_emails = cur.fetchall(); # Fetches output from query
-    if len(all_emails) != 0: 
-    # Checks if the output of query is not 0. If it's not 0 that means the query has found a match
-    # and therefore the email has been used already, i.e. exisiting user.
+    cur.execute("Select email from Users WHERE email=?", (email,))  # Queries database
+    all_emails = cur.fetchall();  # Fetches output from query
+    if len(all_emails) != 0:
+        # Checks if the output of query is not 0. If it's not 0 that means the query has found a match
+        # and therefore the email has been used already, i.e. exisiting user.
         return False
     return True
+
 
 # Home page for website
 # Returns the home.html template which allows the user to use the functions in the website.
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Called when the user wants to add a new user
 # Returns the newuser.html template with the iput form required for getting the data from the user.
@@ -72,11 +77,12 @@ def new_user():
     roles = ['Warehouse', 'Authenticator', 'Customer']
     return render_template("newuser.html", roles=roles)
 
+
 # newuser.html routes the form to this function
 # Organises the data inputted from user for inserting to user database
-@app.route('/adduser',methods = ['POST', 'GET'])
+@app.route('/adduser', methods=['POST', 'GET'])
 def add_user():
-    if request.method =="POST":
+    if request.method == "POST":
         try:
             # Creates user_id using random characters
             user_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
@@ -92,10 +98,12 @@ def add_user():
             # Checks if the user is new
             checkIfUserIsNew = IsUserNew(email)
             date_joined = datetime.now()
-            if checkIfUserIsNew: # If user is new then will add user details to database
+            if checkIfUserIsNew:  # If user is new then will add user details to database
                 with sql.connect("UserDatabase.db") as con:
                     cur = con.cursor()
-                    cur.execute("INSERT INTO Users (user_id,email,first_name,last_name,pword,date_joined,address,city,role) VALUES (?,?,?,?,?,?,?,?,?)",(user_id,email,fn,ln,pword,date_joined,addr,city,role) )
+                    cur.execute(
+                        "INSERT INTO Users (user_id,email,first_name,last_name,pword,date_joined,address,city,role) VALUES (?,?,?,?,?,?,?,?,?)",
+                        (user_id, email, fn, ln, pword, date_joined, addr, city, role))
                     con.commit()
                     msg = "Record successfully added"
             # This executes if user is not new
@@ -109,8 +117,9 @@ def add_user():
             # Outputs the result of the operation
             # 1 of 3 options: Successfully add, not added due to existing user or error
             # Correct message will display depending on which of the 3 options happens
-            return render_template("result.html",msg = msg)
+            return render_template("result.html", msg=msg)
             con.close()
+
 
 # Lists the users in the database
 # For debugging, will not be in the final website
@@ -123,7 +132,8 @@ def list_users():
     cur.execute("select * from Users")
 
     rows = cur.fetchall();
-    return render_template("list.html", rows = rows)
+    return render_template("list.html", rows=rows)
+
 
 # USer login page, calls the check_user_login function
 @app.route('/UserLogin')
@@ -132,10 +142,10 @@ def user_login():
 
 
 # Checks if the users details entered are correct. If the email and password match for the same user then they are logged in succesfully
-@app.route('/login',methods = ['POST'])
+@app.route('/login', methods=['POST'])
 def check_user_login():
     global LOGIN
-    if request.method =="POST":
+    if request.method == "POST":
         try:
             # Gets the information from the user
             email = request.form['email']
@@ -143,14 +153,14 @@ def check_user_login():
             with sql.connect("UserDatabase.db") as con:
                 cur = con.cursor()
                 # Selects the user where email and password match
-                cur.execute("Select * from Users WHERE email=? AND pword=?", (email, pword, ))
+                cur.execute("Select * from Users WHERE email=? AND pword=?", (email, pword,))
                 user_info = cur.fetchall();
                 # If len(user_info) = 1, this means the query has found a user and therefore the user is logged in
                 if len(user_info) == 1:
                     # User is told they are logged in
                     msg = "Successful Login"
                     # Sets the global variable, therefore allowing this to be seen later on
-                    LOGIN = True 
+                    LOGIN = True
                 else:
                     # If anything else is found then the login is unsucessful and so the user is told
                     msg = "Unsuccessful Login"
@@ -160,46 +170,9 @@ def check_user_login():
             msg = "Error in insert operation"
 
         finally:
-            return (render_template("result.html",msg = msg))
+            return (render_template("result.html", msg=msg))
             con.close()
 
-# Checks if the user is logged in and therefore allowed to upload a picture to the s3 bucket
-@app.route('/s3UploadTest')
-def s3_upload():
-    global LOGIN
-    print("----------yoyoyoyo------------")
-    print(LOGIN)
-    
-    if LOGIN: # Checks if LOGIN is true and then allows the user to upload the file
-        return render_template('s3UploadTest.html')
-    else:
-        msg = "Please login first" # If LOGIN is false, the user is asked to login
-        return render_template("result.html",msg = msg)
-
-# # Uploads the file to the s3 bucket
-# @app.route('/upload',methods=['post'])
-# def upload():
-#     global prod_id
-#     if request.method == 'POST':
-#         img = request.files['file'] # Takes the file, in this case apicture
-#         if img:
-#             filename = secure_filename(img.filename) # Gets the file name for the picture
-#             fileNameSplit = filename.split(".") # Splits the filename to get the extension
-#             fileExtention = fileNameSplit[1]
-#             if fileExtention in ALLOWED_EXTENSIONS: # Checks if the extensions are in the allowed extensions
-#                 filename = prod_id + "." + fileExtention
-#                 img.save(filename)
-#                 # Uploads the file to the s3 bucket
-#                 s3.upload_file(
-#                     Bucket = BUCKET_NAME,
-#                     Filename=filename,
-#                     Key = filename
-#                 )
-#                 msg = "Upload Done ! " # Tells the user the upload is complete
-#             else:
-#                 msg = "Not a png/jpg/jpeg." # Error occurs if the extension is not an allowed extension and tells the user this
-
-#     return render_template("result.html",msg =msg)
 
 # For adding new products to inventory
 @app.route('/newproduct')
@@ -208,78 +181,84 @@ def new_product():
     authenticated = ['Yes', 'No']
     return render_template("newproduct.html", authenticated=authenticated)
 
+
 # Using the product id, the product is checked to see if it already exists
 def IsProdNew(prod_id):
-    con = sql.connect("InventoryDatabase.db") # Connects to the inventory Database
+    con = sql.connect("InventoryDatabase.db")  # Connects to the inventory Database
     cur = con.cursor()
-    cur.execute("Select prod_id from Inventory WHERE prod_id=?",(prod_id, )) # Selects the products that have the same ID as the one given
+    cur.execute("Select prod_id from Inventory WHERE prod_id=?",
+                (prod_id,))  # Selects the products that have the same ID as the one given
     all_prod_id = cur.fetchall();
-    if len(all_prod_id) != 0: # If there are some clashes the new product is not added
+    if len(all_prod_id) != 0:  # If there are some clashes the new product is not added
         return False
     return True
 
-def upload(prod_id, img):
+
+def upload(prod_id, img):  # Uses the prod_id to name the picture uploaded to source bucket
     if img:
-        filename = secure_filename(img.filename) # Gets the file name for the picture
-        fileNameSplit = filename.split(".") # Splits the filename to get the extension
+        filename = secure_filename(img.filename)  # Gets the file name for the picture
+        fileNameSplit = filename.split(".")  # Splits the filename to get the extension
         fileExtention = fileNameSplit[1]
-        if fileExtention in ALLOWED_EXTENSIONS: # Checks if the extensions are in the allowed extensions
+        if fileExtention in ALLOWED_EXTENSIONS:  # Checks if the extensions are in the allowed extensions
             filename = prod_id + "." + fileExtention
             img.save(filename)
             # Uploads the file to the s3 bucket
             s3.upload_file(
-                Bucket = BUCKET_NAME,
+                Bucket=(hasAllBucketNamesObject.getSourceBucketName),
                 Filename=filename,
-                Key = filename
+                Key=filename
             )
-            msg = "Upload Done ! " # Tells the user the upload is complete
+            msg = "Upload Done ! "  # Tells the user the upload is complete
         else:
-            msg = "Not a png/jpg/jpeg." # Error occurs if the extension is not an allowed extension and tells the user this
+            msg = "Not a png/jpg/jpeg."  # Error occurs if the extension is not an allowed extension and tells the user this
 
     return msg
 
+
 # Gets the information for the new product from the user via form
-@app.route('/addproduct',methods = ['POST', 'GET'])
+@app.route('/addproduct', methods=['POST', 'GET'])
 def add_product():
     # global prod_id
-    new_object = bucket_names_object.bucket_names_object()
-    if request.method =="POST":
+
+    if request.method == "POST":
         try:
-            
+
             prod_id = request.form['prod_ID']
             prod_name = request.form['prod_name']
             price = request.form['price']
             desc = request.form['desc']
             quantity = request.form['quantity']
             auth = request.form['auth']
-            img = request.files['file'] # Takes the file, in this case apicture
+            img = request.files['file']  # Takes the file, in this case apicture
 
-            checkIfProdIsNew = IsProdNew(prod_id) # Checks if the product is new or exisiting
+            checkIfProdIsNew = IsProdNew(prod_id)  # Checks if the product is new or exisiting
 
             if checkIfProdIsNew:
-                print(prod_id, prod_name, price)
                 msgFromS3Upload = upload(prod_id, img)
-                temp_url = s3_bucket_operations.getURLOfONEObjectWithinAnS3Bucket(new_object.getRefactoredBucketName, prod_id)
-                print(temp_url)
-                print("-----------yoyoyoyoyo----------------")
+                temp_url = s3_bucket_operations.getURLOfONEObjectWithinAnS3Bucket(new_object.getRefactoredBucketName,
+                                                                                  prod_id)
+
                 with sql.connect("InventoryDatabase.db") as con:
                     cur = con.cursor()
                     # Inserts the new product into the database
-                    cur.execute("INSERT INTO Inventory (prod_ID,prod_name,price,desc,quantity,auth,prod_url) VALUES (?,?,?,?,?,?,?)",(prod_id,prod_name,price,desc,quantity,auth,temp_url) )
+                    cur.execute(
+                        "INSERT INTO Inventory (prod_ID,prod_name,price,desc,quantity,auth,prod_url) VALUES (?,?,?,?,?,?,?)",
+                        (prod_id, prod_name, price, desc, quantity, auth, temp_url))
                     con.commit()
-                    msg = "Product successfully added" # Tells the user the outcome
+                    msg = "Product successfully added"  # Tells the user the outcome
                     msg = msg + " and " + msgFromS3Upload
-            if not checkIfProdIsNew: # If the product id is already there, the user will be told and the product is not added
+            if not checkIfProdIsNew:  # If the product id is already there, the user will be told and the product is not added
                 msg = "This is an exsiting product"
         except:
             con.rollback()
             msg = "Error in insert operation"
 
         finally:
-            
-            return render_template("result.html",msg = msg)
+
+            return render_template("result.html", msg=msg)
             con.close()
-  
+
+
 # List the products from the database for the users to see
 @app.route('/listprods')
 def list_prods():
@@ -290,37 +269,24 @@ def list_prods():
     cur.execute("select * from Inventory")
 
     rows = cur.fetchall();
-    print("-------yoyoyo----------")
-    print(rows)
-    return render_template("listprods.html", rows = rows)
+
+    return render_template("listprods.html", rows=rows)
+
 
 @app.route('/devTest')
 # Show future version of what our home page will be
 def devTest():
-
     # START - Dev test stuff
-
     # s3_bucket_operations.getFileNamesOfObjectsWithinAnS3Bucket(nameOfS3BucketToBeCalled = "refactored-image-bucket-5623")
 
     # s3_bucket_operations.getURLsOfAnObjectWithinAnS3Bucket(nameOfS3BucketToBeCalled="source-image-bucket-5623",
     #                                                        nameOfObjectFile="beach.jpg")
 
-    # print(s3_bucket_operations.getAllObjectsURLsFromS3AsList(nameOfS3BucketToBeCalled=hasAllBucketNamesObject.getSourceBucketName))
-
-    # DATABASE STUFFS
-    dbConnection = sql.connect("InventoryDatabase.db")
-    dbConnection.row_factory = sql.Row
-
-    cursor = dbConnection.cursor()
-    cursor.execute("select prod_url from Inventory")
-
-    objectUrlsFromS3 = cursor.fetchall()
-
-    print("\n -------- Database stuff from DEVTEST function ----------")
-    print(objectUrlsFromS3)
+    print(s3_bucket_operations.getAllObjectsURLsFromS3AsList(nameOfS3BucketToBeCalled="source-image-bucket-5623"))
     # END - Dev test stuff
 
-    return render_template('futureHomePage.html', objectUrlsFromS3=objectUrlsFromS3) # todo keep this here
+    return render_template('futureHomePage.html')  # todo keep this here
+
 
 @app.route('/dispImage')
 def disp_image():
@@ -331,4 +297,4 @@ if __name__ == '__main__':
     # global LOGIN
     global LOGIN, prod_id
     LOGIN = False
-    app.run(debug = True) # Starts the app in debug mode
+    app.run(debug=True)  # Starts the app in debug mode
